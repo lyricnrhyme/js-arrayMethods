1124a9d89ed6d6cb10cc4d2edbcfab4717e9e2bb
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-01-05 20:11:00 -1000

    complete exercise 1

--- exercise.js
+++ exercise.js
@@ -13,15 +13,17 @@
 /*1. Mr. Bonner (aka the Red Rocket, the Red Mamba) doesn't belong in this class of Hall Of Famers. Remove this imposter immediately and assign it to a new variable named `notHof` Console.log this new variable as well as the original list.*/ 
 
 var hof = ["Jordan", "Bird", "Kobe", "Duncan", "Iverson", "Bonner"];
-
-
+var notHof = hof.pop();
+console.log (notHof)
+console.log (hof)
 
 /* The array.push() adds one or more element to the end of an array and returns the new length of the array*/
 
 /*2. You've been tasked to do this week's grocery shopping. As you arrive at Times Supermarket, Kanye pings you to get a bottle of Hendricks gin. Add this to the existing shoppingList and console.log it.*/
 
 var shoppingList = ["cool ranch doritos", "kings hawaiian sweet bread", "peanut butter oreos", "fruit loops cereal"];
-
+shoppingList.push("bottle of Hendricks gin");
+console.log(shoppingList)
 
 
 
@@ -30,7 +32,8 @@ var shoppingList = ["cool ranch doritos", "kings hawaiian sweet bread", "peanut 
 /*3. Use the force, or in this case the reverse method to help Yoda make some sense with his motivational talk to the young Jedi interns.*/
 
 var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
-
+yoda.reverse();
+console.log(yoda)
 
 
 /*The array.shift() removes the first element from an array and returns that element*/
